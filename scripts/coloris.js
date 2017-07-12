9b6ba5a84a793c868f1fe9e68c609518c86f1a4c
Jordi Manchon <jordimanchon@me.com>
2017-07-12 16:56:10 +0200

    Added tutorial

--- scripts/coloris.js
+++ scripts/coloris.js
@@ -14,7 +14,30 @@ function renderStart() {
       'align-items': 'center'
     });
   var buttonRow = $('<div>')
-    ;
+    .css({
+      'margin': '0 auto',
+      'display': 'flex',
+      'justify-content': 'center',
+      'align-items': 'center'
+    });
+  var tutorialButton = $('<button>')
+    .addClass('btn')
+    .attr('id', 'btn-tutorial')
+    .attr('type', 'button')
+    .html('<p>?</p>')
+    .css({
+      'background': 'rgb(255, 160, 255)',
+      'margin': '0 2vw',
+      'border-radius': '50%',
+      'padding': 'calc(90vh / ' + trueHeight + ' * 0.3)',
+      'width': '0',
+      'height': '0',
+      'color': 'white',
+      'display': 'flex',
+      'justify-content': 'center',
+      'align-items': 'center',
+      'font-size': '1.2rem'
+    });
   var startButton = $('<button>')
     .addClass('btn')
     .attr('id', 'btn-start')
@@ -22,7 +45,7 @@ function renderStart() {
     .html('<span class="glyphicon glyphicon-play" aria-hidden="true"></span>')
     .css({
       'background': 'rgb(160, 255, 160)',
-      'margin': '0 auto',
+      'margin': '0 2vw',
       'border-radius': '50%',
       'padding': 'calc(90vh / ' + trueHeight + ' * 0.3)',
       'width': '0',
@@ -40,9 +63,145 @@ function renderStart() {
       'bottom': '10vh',
       'transform': 'translate(-50%, 0%)'
     });
-  buttonRow.append(startButton);
+  buttonRow.append(tutorialButton, startButton);
   startPage.append(logo, buttonRow);
   $('.container').append(startPage, footer);
+}
+
+function renderTutorial() {
+  var tutorial = $('<div>')
+    .css({
+
+    });
+  var redCoin = $('<div>')
+    .addClass('col-xs-6 red')
+    .css({
+      'width': '0',
+      'height': '0',
+      'padding': 'calc(90vh / ' + trueHeight + ' * 0.3)',
+      'margin': 'calc(90vh / ' + trueHeight + ' * 0.2)'
+    });
+  var greenCoin = $('<div>')
+    .addClass('col-xs-6 green')
+    .css({
+      'width': '0',
+      'height': '0',
+      'padding': 'calc(90vh / ' + trueHeight + ' * 0.3)',
+      'margin': 'calc(90vh / ' + trueHeight + ' * 0.2)'
+    });
+  var blueCoin = $('<div>')
+    .addClass('col-xs-6 blue')
+    .css({
+      'width': '0',
+      'height': '0',
+      'padding': 'calc(90vh / ' + trueHeight + ' * 0.3)',
+      'margin': 'calc(90vh / ' + trueHeight + ' * 0.2)'
+    });
+  var primaryColorsRow = $('<div>')
+    .addClass('row');
+  primaryColorsRow.append(redCoin, greenCoin, blueCoin);
+  var primaryColors = $('<div>')
+    .html('<p>Mix pairs of primary colors together…</p>')
+    .css({
+      'text-align': 'center',
+      'margin-top': '6vh'
+    })
+    .append(primaryColorsRow);
+  var cyanCoin = $('<div>')
+    .addClass('col-xs-6 cyan')
+    .css({
+      'width': '0',
+      'height': '0',
+      'padding': 'calc(90vh / ' + trueHeight + ' * 0.3)',
+      'margin': 'calc(90vh / ' + trueHeight + ' * 0.2)'
+    });
+  var magentaCoin = $('<div>')
+    .addClass('col-xs-6 magenta')
+    .css({
+      'width': '0',
+      'height': '0',
+      'padding': 'calc(90vh / ' + trueHeight + ' * 0.3)',
+      'margin': 'calc(90vh / ' + trueHeight + ' * 0.2)'
+    });
+  var yellowCoin = $('<div>')
+    .addClass('col-xs-6 yellow')
+    .css({
+      'width': '0',
+      'height': '0',
+      'padding': 'calc(90vh / ' + trueHeight + ' * 0.3)',
+      'margin': 'calc(90vh / ' + trueHeight + ' * 0.2)'
+    });
+  var secondaryColorsRow = $('<div>')
+  .addClass('row')
+  .append(cyanCoin, magentaCoin, yellowCoin);
+  var secondaryColors = $('<div>')
+    .html('<p>…or pairs of secondary colors.</p>')
+    .css({
+      'text-align': 'center',
+      'margin-top': '6vh'
+    })
+    .append(secondaryColorsRow);
+  var silverCoin = $('<div>')
+    .addClass('col-xs-6 silver')
+    .css({
+      'width': '0',
+      'height': '0',
+      'padding': 'calc(90vh / ' + trueHeight + ' * 0.3)',
+      'margin': 'calc(90vh / ' + trueHeight + ' * 0.2)'
+    });
+  var silverCoinsRow = $('<div>')
+    .addClass('row')
+    .append(silverCoin);
+  var silverCoins = $('<div>')
+    .html('<p>Get the silver coins!</p>')
+    .css({
+      'text-align': 'center',
+      'margin-top': '6vh'
+    })
+    .append(silverCoinsRow);
+  var buttons = $('<div>')
+    .css({
+      'margin': '12vh auto',
+      'display': 'flex',
+      'justify-content': 'center',
+      'align-items': 'center'
+    });
+  var startButton = $('<button>')
+    .addClass('btn')
+    .attr('id', 'btn-start')
+    .attr('type', 'button')
+    .html('<span class="glyphicon glyphicon-play" aria-hidden="true"></span>')
+    .css({
+      'background': 'rgb(160, 255, 160)',
+      'margin': '0 2vw',
+      'border-radius': '50%',
+      'padding': 'calc(90vh / ' + trueHeight + ' * 0.3)',
+      'width': '0',
+      'height': '0',
+      'display': 'flex',
+      'justify-content': 'center',
+      'align-items': 'center'
+    })
+    .click(function(){
+      $('.container').empty();
+      renderBoard(board);
+      var cell = $('.color').not($('.silver'));
+      var coin = $('.silver');
+      if (game.canSelect()) {
+        cell.on('click', function(c) {
+          var selectedCell = $(this).data();
+          var selected = $(this)
+           .addClass('selectedCell');
+          game.move(selectedCell);
+          if (game.activeMove.length === 2) {
+            game.endMove();
+          }
+        });
+      }
+    });
+  buttons.append(startButton);
+  tutorial.append(primaryColors, secondaryColors, silverCoins, buttons);
+  $('.container').append(tutorial);
 }
 
 function renderBoard(board) {
@@ -70,6 +229,11 @@ $(document).ready(function(){
   renderStart();
 
   //counters
+
+  $('#btn-tutorial').click(function(){
+    $('.container').empty();
+    renderTutorial();
+  });
 
   $('#btn-start').click(function(){
     $('.container').empty();
